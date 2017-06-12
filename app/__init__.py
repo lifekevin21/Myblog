60fac88b2cdc36fb5d3cdf9eec1ff09e701ef783
lifekevin21 <lifekevin21@126.com>
2017-06-12 17:26:42 +0800

    add file models

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from app import views, models
 
 
 app = Flask(__name__)
 app.config.from_object('config')
-from app import views
+
+# 导入Flask-SQLAlchemy
+# 创建了一个 db 对象，这是我们的数据库
+# 导入一个新的模块，叫做 models
+db = SQLAlchemy(app)
