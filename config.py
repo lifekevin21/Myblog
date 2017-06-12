6690cac7f8d5a4dda7b3a78f6d8da3829b98c0ef
lifekevin21 <lifekevin21@126.com>
2017-06-12 17:18:11 +0800

    modify file config

--- config.py
+++ config.py
@@ -1,2 +1,15 @@
+import os
+
+
 CSRF_ENABLED = True
 SECRET_KEY = 'never-try-this'
+
+
+# db文件创建在与创建脚本同一目录下
+# SQLALCHEMY_DATABASE_URI是the Flask-SQLAlchemy必需的扩展。这是我们的数据库文件的路径。
+# SQLALCHEMY_MIGRATE_REPO 是用来存储SQLAlchemy-migrate数据库文件的文件夹。
+# SQLALCHEMY_TRACK_MODIFICATIONS 不设为True会报错(貌似这段代码没有也能正常运行)
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
